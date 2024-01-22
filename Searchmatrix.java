1516bda3edd5dfa1f786e61b1655e329a3f68b0c
Shreya N <nshreya094@gmail.com>
2024-01-22 19:41:16 +0530

    Searchmatrix

---
+++ Searchmatrix.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Searchmatrix {
+  public static boolean search(int matrix[][] ,int key){
+    for(int i = 0; i<matrix.length; i++){
+      for(int j = 0; j<matrix[0].length; j++){
+        if(matrix[i][j] == key){
+          System.out.println("found at the cell " + "(" + i + "," + j + ")");
+          return true;
+        }
+      }
+    }
+    System.out.println("not found");
+    return false;
+  }
+   public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int matrix[][] = new int[3][3];
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        // Input
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Output
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+      
+        search(matrix ,4);
+    
+    }
+}
