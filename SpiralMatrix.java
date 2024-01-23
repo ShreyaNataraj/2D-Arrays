050dd662878a7bba4e21f98ae3275c7ffdfeca43
Shreya N <nshreya094@gmail.com>
2024-01-23 11:24:31 +0530

    Diagonal sum optimised code

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -36,10 +36,10 @@ public class SpiralMatrix {
   
 }
   public static void main(String[] args) {
-    int matrix[][] = {{1,2,3,4},
-                      {5,6,7,8},
-                      {9,10,11,12},
-                      {13,14,15,16}};
+    int matrix[][] = {{1,2,3,4,5},
+                      {5,6,7,8,4},
+                      {9,10,11,12,7},
+                      {13,14,15,16,8}};
     spiralmatrix(matrix);
     
   }
